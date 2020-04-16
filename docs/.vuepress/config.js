de35155f51fbc257acd99548e16a4466ee77f91b
weimaotao <434402748@qq.com>
2020-04-16 17:45:03 +0800

    笔记

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  // base: "/",
+  title: "个人博客", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+  description: "个人博客,个人前端记录", // meta 中的描述文字，用于SEO
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ["link", { rel: "icon", href: "/timg.jpg" }] //浏览器的标签栏的网页图标
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  serviceWorker: true,
+  themeConfig: {
+    logo: "/timg.jpg",
+    lastUpdated: "lastUpdate", // string | boolean
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "分类",
+        ariaLabel: "分类",
+        items: [
+          { text: "文章", link: "/pages/folder1/test1.md" },
+          { text: "琐碎", link: "/pages/folder2/test4.md" }
+        ]
+      },
+      { text: "功能演示", link: "/pages/folder1/test3.md" },
+      { text: "Github", link: "https://github.com/dwanda" }
+    ],
+    // sidebar: 'auto'
+    sidebar: {
+      "/pages/folder1/": [
+        {
+          title: "测试菜单1", // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            ["test1.md", "子菜单1"],
+            ["test3.md", "子菜单2"],
+            ["test6.md", "子菜单3"]
+          ]
+        },
+        {
+          title: "测试菜单2",
+          collapsable: false, // 可选的, 默认值是 true,
+          children: [["test2.md", "子菜单1"]]
+        }
+      ]
+    }
+    // serviceWorker: {
+    // //   updatePopup: true // Boolean | Object, 默认值是 undefined.
+    //   // 如果设置为 true, 默认的文本配置将是:
+    //   updatePopup: {
+    //      message: "New content is available.",
+    //      buttonText: "Refresh"
+    //   }
+    // }
+  }
+};
